c08730b66974b2bd27560d195b3ff65b34684b53
Johan <hal215@outlook.com>
2021-01-09 05:10:56 -0600

    script done. Works like a chaaarm, may need user efort in adding newlines between the verses though

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+websiteLink = input("Please enter your uta-net URL: ")
+r = requests.get(websiteLink)
+soup = BeautifulSoup(r.content, 'html.parser', from_encoding="utf-8")
+interest = soup.find("div", {"id" : "kashi_area"});
+splitInterest = interest.prettify().split("\n");
+
+lyrics = [x for x in splitInterest if "<" not in x]
+lyrics = list(map(lambda x: x.strip(),lyrics))
+
+fileName = input("\n What would you like to name the lyrics .txt? ").strip();
+
+with open(fileName+".txt", encoding="utf-8", mode="w") as f:
+	for line in lyrics:
+		f.write(line+"\n");
+
+print("File created.")
+
